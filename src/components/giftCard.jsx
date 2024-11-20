b07657e0526d1bf8fef570544d9352f0ef9c2e0e
Ethan <elephant9000@gmail.com>
2024-11-19 23:53:31 -0500

    update

--- src/components/giftCard.jsx
+++ src/components/giftCard.jsx
@@ -15,30 +15,40 @@ const GiftCard = ({ gift, index }) => {
 
   return (
     <div className="grid grid-cols-1 lg:grid-cols-6 gap-2 lg:gap-4">
-      <a
-        href={gift.url}
-        target="_blank"
-        className={`text-xl lg:text-2xl col-span-4 ${
-          gift.purchased ? "line-through" : ""
-        }`}
-      >
-        {gift.name}
-      </a>
-      <div className="flex gap-2 col-span-2 lg:justify-end">
-        {!gift.purchased && (
-          <button onClick={handleMarkAsPurchased} className="underline text-xs">
-            Mark as purchased
-          </button>
-        )}
-        {gift.purchased && (
-          <button
-            onClick={handleUndoMarkAsPurchased}
-            className="underline text-xs"
-          >
-            Undo
-          </button>
-        )}
-      </div>
+      {gift.purchasable && (
+        <a
+          href={gift.url}
+          target="_blank"
+          className={`text-xl lg:text-2xl col-span-4 ${
+            gift.purchased ? "line-through" : ""
+          }`}
+        >
+          {gift.name}
+        </a>
+      )}
+      {!gift.purchasable && (
+        <h2 className="text-xl lg:text-2xl col-span-4">{gift.name}</h2>
+      )}
+      {gift.purchasable && (
+        <div className="flex gap-2 col-span-2 lg:justify-end">
+          {!gift.purchased && (
+            <button
+              onClick={handleMarkAsPurchased}
+              className="underline text-xs"
+            >
+              Mark as purchased
+            </button>
+          )}
+          {gift.purchased && (
+            <button
+              onClick={handleUndoMarkAsPurchased}
+              className="underline text-xs"
+            >
+              Undo
+            </button>
+          )}
+        </div>
+      )}
     </div>
   );
 };
