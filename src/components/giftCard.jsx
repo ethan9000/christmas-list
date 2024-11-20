5395290223e85ccbdb0e055491148a340f7cbbbd
Ethan <elephant9000@gmail.com>
2024-11-19 22:56:26 -0500

    adds link

--- src/components/giftCard.jsx
+++ src/components/giftCard.jsx
@@ -15,13 +15,15 @@ const GiftCard = ({ gift, index }) => {
 
   return (
     <div className="grid grid-cols-1 lg:grid-cols-6 gap-2 lg:gap-4">
-      <h2
+      <a
+        href={gift.url}
+        target="_blank"
         className={`text-xl lg:text-2xl col-span-4 ${
           gift.purchased ? "line-through" : ""
         }`}
       >
         {gift.name}
-      </h2>
+      </a>
       <div className="flex gap-2 col-span-2 lg:justify-end">
         {!gift.purchased && (
           <button onClick={handleMarkAsPurchased} className="underline text-xs">
